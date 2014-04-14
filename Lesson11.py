df2644f9f4ee7dfd7fae019b687ce98141a3c3c6
colinparker <colinparker@gwu.edu>
2014-04-14 13:14:55 -0400

    Panels Entered

--- Lesson11.py
+++ Lesson11.py
@@ -19,3 +19,67 @@ plt.ylabel('y',fontsize=16)
 plt.xlim(xStart,xEnd)
 plt.ylim(yStart,yEnd)
 plt.plot(xp,yp,'k',linewidth=2);
+
+class Panel:
+    def __init__(self,xa,ya,xb,yb):
+        self.xa,self.ya=xa,ya
+        self.xb,self.yb=xb,yb
+        self.xc,self.yc=(xa+xb)/2,(ya+yb)/2
+        self.length=sqrt((xb-xa)**2+(yb-ya)**2)
+        
+        if(xb-xa<=0):self.beta=acos((yb-ya)/self.length)
+        elif (xb-xa>0):self.beta=pi+acos(-(yb-ya)/self.length)
+        
+        if(self.beta<=pi):self.loc='extrados'
+        else:self.loc='intrados'
+        
+        self.sigma=0.
+        self.vt=0.
+        self.Cp=0.
+
+def definePanels(N,xp,yp):
+    
+    R=(max(xp)-min(xp))/2
+    xCenter =(max(xp)+min(xp))/2
+    xCircle=xCenter + R*np.cos(np.linspace(0,2*pi,N+1))
+    
+    x=np.copy(xCircle)
+    y=np.empty_like(x)
+    
+    xp,yp=np.append(xp,xp[0]),np.append(yp,yp[0])
+    
+    I=0
+    for i in range(N):
+        while(I<len(xp)-1):
+            if(xp[I]<=x[i]<=xp[I+1] or xp[I+1]<=x[i]<=xp[I]):break
+            else:I+=1
+        a=(yp[I+1]-yp[I])/(xp[I+1]-xp[I])
+        b=yp[I+1]-a*xp[I+1]
+        y[i]=a*x[i]+b
+    y[N]=y[0]
+    
+    panel=np.empty(N,dtype=object)
+    for i in range(N):
+        panel[i]=Panel(x[i],y[i],x[i+1],y[i+1])
+    return panel
+
+N=20
+panel=definePanels(N,xp,yp)
+
+valX,valY = 0.1,0.2
+xmin,xmax = min([p.xa for p in panel]),max([p.xa for p in panel])
+ymin,ymax = min([p.ya for p in panel]),max([p.ya for p in panel])
+xStart,xEnd = xmin-valX*(xmax-xmin),xmax+valX*(xmax-xmin)
+yStart,yEnd = ymin-valY*(ymax-ymin),ymax+valY*(ymax-ymin)
+size = 10
+plt.figure(figsize=(size,(yEnd-yStart)/(xEnd-xStart)*size))
+p#lt.grid(True)
+plt.xlabel('x',fontsize=16)
+plt.ylabel('y',fontsize=16)
+plt.xlim(xStart,xEnd)
+plt.ylim(yStart,yEnd)
+plt.plot(xp,yp,'k-',linewidth=2)
+plt.plot(np.append([p.xa for p in panel],panel[0].xa),\
+        np.append([p.ya for p in panel],panel[0].ya),\
+        linestyle='-',linewidth=1,\
+        marker='o',markersize=6,color='#CD2305');
